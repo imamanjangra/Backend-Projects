858b8d4ed4de8f3924438a9f4476ac81cfd631b3
imamanjangra <imamanjangra@gmail.com>
2026-03-06 17:26:48 +0530

    initialize frontend with React, Vite, and Tailwind CSS

---
+++ URL shortener/frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './pages/login.jsx'
+import Signup from './pages/Signup'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        {/* <Login/> */}
+        <Signup/>   
+      </div>
+    </>
+  )
+}
+
+export default App
